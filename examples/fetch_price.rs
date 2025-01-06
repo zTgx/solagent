9a56ba10e442b2fea11688e2a59f3840d3d1fdb6
zTgx <747674262@qq.com>
2025-01-06 19:09:43 +0800

    fix issue 1 : using Pro instead

--- examples/fetch_price.rs
+++ examples/fetch_price.rs
@@ -1,22 +1,22 @@
 use rig::{
     completion::Prompt,
-    providers::gemini::{self, completion::GEMINI_1_5_FLASH},
+    providers::gemini::{self, completion::GEMINI_1_5_PRO},
 };
 use solagent::{fetch_price::FetchPrice, SolAgent};
 
 #[tokio::main]
 async fn main() {
     // TODO: bug here: https://github.com/zTgx/solagent.rs/issues/1
-    // let token_id = "So11111111111111111111111111111111111111112";
+    let token_id = "So11111111111111111111111111111111111111112";
 
-    let token_id = "JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN";
+    // let token_id = "JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN";
     let price = SolAgent::fetch_price(token_id).await.unwrap();
     println!("Price: {}", price);
 
     let fetch_price_tool = FetchPrice;
     let client = gemini::Client::from_env();
     let agent = client
-        .agent(GEMINI_1_5_FLASH)
+        .agent(GEMINI_1_5_PRO)
         .preamble("You are an assistant here to help the user select which tool is most appropriate to perform operations.")
         .max_tokens(1024)
         .tool(fetch_price_tool)
