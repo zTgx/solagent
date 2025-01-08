322981d8d03cdfa2b9ac6eecfde80c71e9ca340b
zTgx <747674262@qq.com>
2025-01-08 13:12:09 +0800

    refact: add new for CollectionOptions

--- examples/deploy_collection.rs
+++ examples/deploy_collection.rs
@@ -1,21 +1,18 @@
-use mpl_token_metadata::types::Creator;
 use solagent::{CollectionOptions, SolAgent};
 use solana_sdk::pubkey::Pubkey;
 
 #[tokio::main]
 async fn main() {
-    let options = CollectionOptions {
-        name: "Solagent Collection".to_string(),
-        uri: "https://img.zol.com.cn/group/217/a2167467.jpg".to_string(),
-        royalty_basis_points: Some(500),
-        creators: Some(vec![Creator {
-            address: Pubkey::from_str_const("HHV3DX4UT4u3vBek2XCaZeAyox88zuhWfcLRJbFx1oYt"),
-            verified: true,
-            share: 100,
-        }]),
-    };
+    let name = "Solagent Collection";
+    let uri = "https://arweave.net/metadata.json";
+    let royalty_basis_points = Some(500);
+    let creators = vec![(
+        Pubkey::from_str_const("HHV3DX4UT4u3vBek2XCaZeAyox88zuhWfcLRJbFx1oYt"),
+        100,
+    )];
+    let options = CollectionOptions::new(name, uri, royalty_basis_points, Some(creators));
 
     let agent = SolAgent::new();
-    let tx = agent.deploy_collection(options).await;
-    println!(">>> deploy collection tx: {:?}", tx);
+    let tx = agent.deploy_collection(options).await.unwrap();
+    println!("Mint: {:?}", tx.0);
 }
--- src/primitives/token.rs
+++ src/primitives/token.rs
@@ -1,4 +1,5 @@
 use mpl_token_metadata::types::Creator;
+use solana_sdk::pubkey::Pubkey;
 
 /// Options for deploying an NFT collection.
 ///
@@ -9,8 +10,35 @@ use mpl_token_metadata::types::Creator;
 /// - `royalty_basis_points`: Optional. The royalty basis points for the collection.
 /// - `creators`: Optional. A list of creators associated with the collection.
 pub struct CollectionOptions {
-    pub name: String,
-    pub uri: String,
-    pub royalty_basis_points: Option<u16>, // Optional royalty basis points
-    pub creators: Option<Vec<Creator>>,    // Optional list of creators
+    pub(crate) name: String,
+    pub(crate) uri: String,
+    pub(crate) royalty_basis_points: Option<u16>, // Optional royalty basis points
+    pub(crate) creators: Option<Vec<Creator>>,    // Optional list of creators
+}
+
+impl CollectionOptions {
+    pub fn new(
+        name: &str,
+        uri: &str,
+        royalty_basis_points: Option<u16>,
+        creators: Option<Vec<(Pubkey, u8)>>,
+    ) -> Self {
+        let creators = creators.map(|creator_tuples| {
+            creator_tuples
+                .into_iter()
+                .map(|(pubkey, share)| Creator {
+                    address: pubkey,
+                    verified: true,
+                    share,
+                })
+                .collect::<Vec<Creator>>()
+        });
+
+        CollectionOptions {
+            name: name.to_string(),
+            uri: uri.to_string(),
+            royalty_basis_points,
+            creators,
+        }
+    }
 }
