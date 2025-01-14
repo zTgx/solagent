d498e8880f4eb0be65b15f8a718f2332281aea8d
zTgx <747674262@qq.com>
2025-01-14 11:27:55 +0800

    example: update get_balance

--- examples/basic/get_balance.rs
+++ examples/basic/get_balance.rs
@@ -17,7 +17,7 @@ use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
-    let agent = Arc::new(SolAgent::new("private_key", "https://api.devnet.solana.com", "openai_api_key"));
+    let agent = Arc::new(SolAgent::new("private_key", "PRC_URL", "openai_api_key"));
     let balance = agent.get_balance(None).await.unwrap();
     println!("My balance: {}", balance);
 }
