cc0d3f0dd2dd0f310c82542133bbdf7f1bfa34fe
zTgx <747674262@qq.com>
2025-01-25 00:49:17 +0800

    refect: update macro_rules! parameters_json_schema

--- src/tools/helius/create_webhook.rs
+++ src/tools/helius/create_webhook.rs
@@ -16,7 +16,6 @@ use crate::{
     actions::{create_webhook, HeliusWebhookResponse},
     parameters_json_schema, SolanaAgentKit,
 };
-use quote::quote;
 use rig::{
     completion::ToolDefinition,
     tool::{Tool, ToolEmbedding},
@@ -24,7 +23,6 @@ use rig::{
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::sync::Arc;
-use syn::parse_quote;
 
 #[derive(Deserialize)]
 pub struct CreateWebHookArgs {
@@ -105,10 +103,7 @@ impl ToolEmbedding for CreateWebHook {
     }
 
     fn embedding_docs(&self) -> Vec<String> {
-        vec![
-            "Get the balance of a Solana wallet or token account which is different from the agent's wallet.".into(),
-            "If no tokenAddress is provided, the SOL balance of the wallet will be returned.".into(),
-        ]
+        vec!["Creates a new webhook in the Helius system to monitor transactions for specified account addresses".into()]
     }
 
     fn context(&self) -> Self::Context {}
--- src/utils/macros.rs
+++ src/utils/macros.rs
@@ -12,29 +12,52 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use serde_json::{json, Value};
+
+pub fn get_json_type(ty: &syn::Type) -> Value {
+    match ty {
+        syn::Type::Path(type_path) => {
+            let segment = &type_path.path.segments[0];
+            let type_name = segment.ident.to_string();
+
+            match type_name.as_str() {
+                "Vec" => {
+                    if let syn::PathArguments::AngleBracketed(args) = &segment.arguments {
+                        if let syn::GenericArgument::Type(inner_type) = &args.args[0] {
+                            let inner_json_type = get_json_type(inner_type);
+                            return json!({
+                                "type": "array",
+                                "items": inner_json_type
+                            });
+                        }
+                    }
+                    json!({ "type": "object" })
+                }
+                "i8" | "i16" | "i32" | "i64" | "u8" | "u16" | "u32" | "u64" | "f32" | "f64" => {
+                    json!({ "type": "number" })
+                }
+                "String" | "str" => {
+                    json!({ "type": "string" })
+                }
+                "bool" => {
+                    json!({ "type": "boolean" })
+                }
+                _ => {
+                    json!({ "type": "object" })
+                }
+            }
+        }
+        _ => json!({ "type": "object" }),
+    }
+}
+
 #[doc(hidden)]
 #[macro_export]
 macro_rules! parameters_json_schema {
-    ($($name:ident: $type:tt),* $(,)?) => {{
+    ($($name:ident: $type:ty),* $(,)?) => {{
         let mut properties = serde_json::Map::new();
         $(
-            let property = match stringify!($type) {
-                "String" => json!({
-                    "type": "string"
-                }),
-                "i32" | "i64" | "u64" | "u32" => json!({
-                    "type": "number"
-                }),
-                "bool" => json!({
-                    "type": "boolean"
-                }),
-
-                _ => {
-                    json!({
-                        "type": "object"
-                    })
-                }
-            };
+            let property = crate::utils::macros::get_json_type(&syn::parse_quote!(<$type>));
             properties.insert(stringify!($name).to_string(), property);
         )*
         json!({
