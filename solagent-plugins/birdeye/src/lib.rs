50258cac892b6e53b224fe435c1335b83566cf3b
zTgx <747674262@qq.com>
2025-02-11 23:02:27 +0800

    feat: add plugin birdeye

--- solagent-plugins/birdeye/src/lib.rs
+++ solagent-plugins/birdeye/src/lib.rs
@@ -1,14 +1,27 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use solagent_core::{IWallet, SolanaAgentKit};
+use std::error::Error;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod primitive;
+pub use primitive::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub async fn get_token_overview<W: IWallet>(
+    agent: &SolanaAgentKit<W>,
+    address: &str,
+) -> Result<Response, Box<dyn Error>> {
+    let api_key =
+        agent.config.birdeye_api_key.as_ref().ok_or("Missing Birdeye API key in agent.config.birdeye_api_key")?;
+
+    let client = reqwest::Client::new();
+    let url = format!("{}/defi/token_overview", BIRDEYE_URL);
+
+    let resp = client
+        .get(url)
+        .query(&[("address", address)])
+        .header("X-API-KEY", api_key)
+        .header("accept", "application/json")
+        .header("x-chain", "solana")
+        .send()
+        .await?;
+
+    Ok(resp.json::<Response>().await?)
 }
