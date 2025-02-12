8dc49943457abd120126d15e3d96523ab99a08e5
zTgx <747674262@qq.com>
2025-02-12 23:47:23 +0800

    chore: SolanaAgentKit<W: IWallet> can not using tool maco

--- solagent-adapters/rig/birdeye/src/lib.rs
+++ solagent-adapters/rig/birdeye/src/lib.rs
@@ -1,20 +1,104 @@
-use anyhow::Result;
-use rig_tool_macro::tool;
-use solagent_core::{IWallet, SolanaAgentKit};
+use serde::{Deserialize, Serialize};
+use solagent_core::{
+    rig::{completion::ToolDefinition, tool::Tool},
+    IWallet, SolanaAgentKit,
+};
+use solagent_parameters::parameters;
 use solagent_plugin_birdeye::{get_market_data, get_token_overview, MarketDataResponse, TokenOverviewResponse};
+use std::sync::Arc;
 
-#[tool(description = "Get market data of single token by birdeye api")]
-pub async fn get_market_data_by_birdeye<W: IWallet>(
-    agent: SolanaAgentKit<W>,
+#[derive(Debug, Deserialize)]
+pub struct MarketDataArgs {
     address: String,
-) -> Result<GetTransactionResponse> {
-    get_market_data(&agent, &address).await
 }
 
-#[tool(description = "Get overview of a token by birdeye api")]
-pub async fn get_token_overview_by_birdeye<W: IWallet>(
-    agent: SolanaAgentKit<W>,
+#[derive(Deserialize, Serialize)]
+pub struct MarketDataOutput {
+    pub data: MarketDataResponse,
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("MarketData error")]
+pub struct MarketDataError;
+
+pub struct MarketData<W: IWallet> {
+    agent: Arc<SolanaAgentKit<W>>,
+}
+
+impl<W: IWallet> MarketData<W> {
+    pub fn new(agent: Arc<SolanaAgentKit<W>>) -> Self {
+        MarketData { agent }
+    }
+}
+
+impl<W: IWallet + std::marker::Send + std::marker::Sync> Tool for MarketData<W> {
+    const NAME: &'static str = "get_market_data";
+
+    type Error = MarketDataError;
+    type Args = MarketDataArgs;
+    type Output = MarketDataOutput;
+
+    async fn definition(&self, _prompt: String) -> ToolDefinition {
+        ToolDefinition {
+            name: "get_market_data".to_string(),
+            description: "Get market data of single token by birdeye api".to_string(),
+            parameters: parameters!(
+                address: String,
+            ),
+        }
+    }
+
+    async fn call(&self, args: Self::Args) -> Result<Self::Output, Self::Error> {
+        let data = get_market_data(&self.agent, &args.address).await.expect("get_market_data");
+
+        Ok(MarketDataOutput { data })
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TokenOverviewArgs {
     address: String,
-) -> Result<GetTransactionResponse> {
-    get_token_overview(&agent, &address).await
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct TokenOverviewOutput {
+    pub data: TokenOverviewResponse,
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("MarketData error")]
+pub struct TokenOverviewError;
+
+pub struct TokenOverview<W: IWallet> {
+    agent: Arc<SolanaAgentKit<W>>,
+}
+
+impl<W: IWallet> TokenOverview<W> {
+    pub fn new(agent: Arc<SolanaAgentKit<W>>) -> Self {
+        TokenOverview { agent }
+    }
+}
+
+impl<W: IWallet + std::marker::Send + std::marker::Sync> Tool for TokenOverview<W> {
+    const NAME: &'static str = "get_token_overview";
+
+    type Error = TokenOverviewError;
+    type Args = TokenOverviewArgs;
+    type Output = TokenOverviewOutput;
+
+    async fn definition(&self, _prompt: String) -> ToolDefinition {
+        ToolDefinition {
+            name: "get_token_overview".to_string(),
+            description: "Get overview of a token by birdeye api".to_string(),
+            parameters: parameters!(
+                address: String,
+            ),
+        }
+    }
+
+    async fn call(&self, args: Self::Args) -> Result<Self::Output, Self::Error> {
+        let data = get_token_overview(&self.agent, &args.address).await.expect("get_token_overview");
+
+        Ok(TokenOverviewOutput { data })
+    }
 }
