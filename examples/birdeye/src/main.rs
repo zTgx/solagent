6d0f4862de8b853b410e9a876b7f7bc85cfad4f2
zTgx <747674262@qq.com>
2025-02-19 07:35:49 +0800

    fix: solana-net-utils bug by using 1.83.0

--- examples/birdeye/src/main.rs
+++ examples/birdeye/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use solagent_core::{ConfigBuilder, SolanaAgentKit};
+use solagent_plugin_birdeye::get_token_metadata;
+use solagent_wallet_solana::Wallet;
+
+#[tokio::main]
+async fn main() {
+    let wallet = Wallet::from_env("variable_name").unwrap();
+
+    let config = ConfigBuilder::default().birdeye_api_key("".into()).build();
+    let agent = SolanaAgentKit::new(wallet, "https://api.devnet.solana.com", config);
+
+    let data = get_token_metadata(&agent, "So11111111111111111111111111111111111111112")
+        .await
+        .unwrap();
+    println!("{:#?}", data);
 }
