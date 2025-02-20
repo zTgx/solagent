9c19a6ef84e5a15fb2e5085828180349536a499a
zTgx <747674262@qq.com>
2025-02-20 14:23:40 +0800

    examples: update birdeye

--- examples/birdeye/src/main.rs
+++ examples/birdeye/src/main.rs
@@ -1,16 +1,27 @@
-use solagent_core::{ConfigBuilder, SolanaAgentKit};
-use solagent_plugin_birdeye::get_token_metadata;
+use solagent_core::{ConfigBuilder, SolanaAgentKit, solana_sdk::signature::Keypair};
+use solagent_plugin_birdeye::{get_token_holders, TokenHolderQueryParams};
 use solagent_wallet_solana::Wallet;
+use std::env;
 
 #[tokio::main]
 async fn main() {
-    let wallet = Wallet::from_env("variable_name").unwrap();
+    let keypair = Keypair::new();
+    let private_key = keypair.to_base58_string();
+    env::set_var("TEST_PRIVATE_KEY", &private_key);
 
-    let config = ConfigBuilder::default().birdeye_api_key("".into()).build();
+    let wallet = Wallet::from_env("TEST_PRIVATE_KEY").unwrap();
+
+    let birdeye_api_key =
+            env::var("BIRDEYE_API_KEY").unwrap();
+
+    let config = ConfigBuilder::default().birdeye_api_key(birdeye_api_key).build();
     let agent = SolanaAgentKit::new(wallet, "https://api.devnet.solana.com", config);
 
-    let data = get_token_metadata(&agent, "So11111111111111111111111111111111111111112")
-        .await
-        .unwrap();
-    println!("{:#?}", data);
+    let address = "So11111111111111111111111111111111111111112";
+    let query = TokenHolderQueryParams::new(address.to_string(), None, None);
+    let overview = get_token_holders(&agent, query).await.unwrap();
+    println!("{:#?}", overview);
+
+
+
 }
